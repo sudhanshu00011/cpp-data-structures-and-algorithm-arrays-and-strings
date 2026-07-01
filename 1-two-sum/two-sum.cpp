d449bccf9d66b6fb103b35fdfcc013fd60cc32ec
Sudhanshu Prasad <146873937+sudhanshu00011@users.noreply.github.com>
2026-07-01 13:40:08 +0530

    Time: 81 ms (9.01%) | Memory: 13.9 MB (99.80%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+//erd
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i<nums.size() ; i++) {
+            for ( int j=0 ; j<=i; j++) {
+                if (i==j) {
+                    continue;
+                }
+                else if (nums[i]+nums[j] == target) {
+                    return {i,j};
+                } 
+            }
+        }
+        return {};
+    }
+};
