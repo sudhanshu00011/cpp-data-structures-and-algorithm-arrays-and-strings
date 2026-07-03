5d0cc0996df61d347d494a84e13f1360de29c7af
Sudhanshu Prasad <146873937+sudhanshu00011@users.noreply.github.com>
2026-07-03 16:50:43 +0530

    Add lengthOfLongestSubstring method in Solution class
    Implement method to find length of longest substring without repeating characters.

--- Longest Substring without repeating characters/longest-substring.cpp
+++ Longest Substring without repeating characters/longest-substring.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector <int> v;
+        for (int i=0; i<s.length(); i++) {
+            if (find(v.begin(), v.end(), s[i]) != v.end()) {
+                return v.size();
+            }
+            else v.push_back(s[i]);
+        }
+        return 0;
+    }
+};
