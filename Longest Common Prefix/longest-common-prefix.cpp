04177c2b1b6ccb87be8d7f7cc31acbdb4b307b2a
Sudhanshu Prasad <146873937+sudhanshu00011@users.noreply.github.com>
2026-07-03 20:46:18 +0530

    Implement longest common prefix function

---
+++ Longest Common Prefix/longest-common-prefix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans = "";
+        for (int i = 0; i < strs[0].size(); i++) {
+            char ch = strs[0][i];
+            for (int j = 1; j < strs.size(); j++) {
+                if (strs[j][i] != ch) return ans;
+            }
+            ans.push_back(ch);
+        }
+        return ans;
+    }
+};
