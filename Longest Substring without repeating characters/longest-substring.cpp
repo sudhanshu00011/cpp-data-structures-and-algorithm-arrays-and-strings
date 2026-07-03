4e684f206a6413780f66934b88188ab7de08ebbb
Sudhanshu Prasad <146873937+sudhanshu00011@users.noreply.github.com>
2026-07-03 17:50:22 +0530

    Optimize longest substring calculation
    Refactor lengthOfLongestSubstring method to use unordered_set for better performance.

--- Longest Substring without repeating characters/longest-substring.cpp
+++ Longest Substring without repeating characters/longest-substring.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector <int> v;
-        for (int i=0; i<s.length(); i++) {
-            if (find(v.begin(), v.end(), s[i]) != v.end()) {
-                return v.size();
+        unordered_set<char> st;
+        int left = 0;
+        int ans = 0;
+        for (int right = 0; right < s.length(); right++) {
+            while (st.find(s[right]) != st.end()) {
+                st.erase(s[left]);
+                left++;
             }
-            else v.push_back(s[i]);
+            st.insert(s[right]);
+            ans = max(ans, right - left + 1);
         }
-        return 0;
+        return ans;
     }
 };
