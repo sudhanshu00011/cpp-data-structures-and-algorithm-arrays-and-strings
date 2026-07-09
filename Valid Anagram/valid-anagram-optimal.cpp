175b865fa23535e074fabebf732d1a37cf10f62e
Sudhanshu Prasad <146873937+sudhanshu00011@users.noreply.github.com>
2026-07-09 11:01:44 +0530

    Implement optimal solution for valid anagram check

---
+++ Valid Anagram/valid-anagram-optimal.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) return false;
+        int freq[26] = {0};
+        for (int i = 0; i < s.size(); i++) {
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
+        }
+        for (int x : freq) {
+            if (x != 0) return false;
+        }
+        return true;
+    }
+};
